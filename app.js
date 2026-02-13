f7c016fda76ac79c966bf8d1bc613847b52c9ba9
amithacks7 <amitprajapati0711@gmail.com>
2026-02-13 10:16:29 +0530

    Backend REST APIs with middleware and validation

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const productRoutes = require("./routes/productRoute");
+const userRoutes = require("./routes/userRoute");
+const cartRoutes = require("./routes/cartRoute");
+const orderRoutes = require("./routes/orderRoute");
+
+app.use(express.json()); // middleware
+
+app.use("/products", productRoutes);
+app.use("/users", userRoutes);
+app.use("/cart", cartRoutes);
+app.use("/orders", orderRoutes);
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
